71917771a202e8064a50d00100e16698f2405d10
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2026-01-27 16:07:38 -0300

    fix: remover CORS com origem fixa (localhost)

--- src/main/java/github/iocentral_de_adocao_if/demo/config/CorsConfig.java
+++ src/main/java/github/iocentral_de_adocao_if/demo/config/CorsConfig.java
@@ -10,12 +10,14 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:63342",
-                        "http://localhost:3000"
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "http://127.0.0.1:*",
+                        "http://192.168.*.*:*"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
